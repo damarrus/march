0898a449871afcd18d016b36d054c2f4fed48554
Nikita Belousov <n.belousov@corp.nekki.ru>
2024-03-02 13:18:00 +0400

    Update icons.ts

--- src/icons.ts
+++ src/icons.ts
@@ -6,6 +6,11 @@ export const ICON_DICE = '🎲';
 export const ICON_GOLD = '💰';
 export const ICON_ACTION = '🏃‍♀️';
 export const ICON_DECLINE = '❌';
+export const ICON_SLEEP = '💤';
+export const ICON_STRENGTH = '💪🏾';
+export const ICON_INTELLECT = '🧠';
+export const ICON_STEAL = '🥷';
+
 
 export const ICON_WARRIOR = '🗡'
 export const ICON_CLERIC = '🕯'
